2dc82b0f10f53d48896a9348f8b88a501be51fc7
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 13:53:14 -0400

    Refactor: simplified and fixed garden controller

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,8 +1,9 @@
 class Garden < ApplicationRecord
   has_many :plots
+
   def unique_plants_under_100_days
     Plant.joins(plant_plots: { plot: :garden })
-         .where(gardens: { id: })
+         .where('gardens.id = ?', id)
          .where('plants.days_to_harvest < ?', 100)
          .distinct
   end
