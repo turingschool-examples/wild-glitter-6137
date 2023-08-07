4a033256d452e8b4e77512cf01ceac3ba2bca465
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 11:50:41 -0400

    Test: adds tests for user story 1 models

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -1,7 +1,9 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Plot, type: :model do
   describe 'relationships' do
     it { should belong_to(:garden) }
+    it { should have_many(:plant_plots) }
+    it { should have_many(:plants).through(:plant_plots) }
   end
 end
