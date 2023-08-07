bfcacc01eda933edd99552e7e338e0efd1ab86c3
Allen Russell <garyrusselljr@gmail.com>
2023-08-07 10:36:31 -0600

    feat: destroy function in plants controller does not effect other plot

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :plots, only: [:index]
+  resources :plots, only: [:index] do
+    resources :plants, only: [:destroy]
+  end
+
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe "Plots Index Page" do
 
   describe "as a visitor" do
     describe "when I visit the plots index page ('/plots')" do
+      # User Story 1
       it "I see a list of all plot numbers, and under each plot number I see the names of all that plot's plants" do
         expect(page).to have_content(@plot1.number)
         expect(page).to have_content(@plot2.number)
@@ -33,6 +34,29 @@ RSpec.describe "Plots Index Page" do
 
         @plot2.plants.each do |plant|
           expect(page).to have_content(plant.name)
+        end
+      end
+
+      # User Story 2
+      it "next to each plant's name I see a link to remove that plant from that plot" do
+        @plot1.plants.each do |plant|
+          expect(page).to have_button("Remove #{plant.name}")
+        end
+      end
+
+      it "when I click on that link I'm returned to the plots index page and no longer see that plant listed under that plot, and I still see that plant's name under other plots that it was assigned to" do
+        expect(page).to have_content(@plant1.name)
+        within("#plot-#{@plot1.id}") do
+          click_on ("Remove #{@plant1.name}")
+        end
+
+        expect(page).to have_current_path(plots_path)
+        within("#plot-#{@plot1.id}") do
+          expect(page).not_to have_content(@plant1.name)
+        end
+    
+        within("#plot-#{@plot2.id}") do
+          expect(page).to have_content(@plant1.name)
         end
       end
     end
