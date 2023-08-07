849e43c3515af6cf2175b16027bc350b73d4a6ba
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 11:51:17 -0400

    Chore: adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+PlantPlot.destroy_all
+Plot.destroy_all
+Garden.destroy_all
+Plant.destroy_all
+
+garden1 = Garden.create!(name: 'Turing Community Garden', organic: true)
+garden2 = Garden.create!(name: 'Main Street Garden', organic: false)
+
+plot1 = Plot.create!(number: 25, size: 'Large', direction: 'East', garden: garden1)
+plot2 = Plot.create!(number: 10, size: 'Small', direction: 'North', garden: garden1)
+plot3 = Plot.create!(number: 30, size: 'Medium', direction: 'East', garden: garden2)
+
+plant1 = Plant.create!(name: 'Tomato', description: 'Easy to care for, generates sauce', days_to_harvest: 90)
+plant2 = Plant.create!(name: 'Eggplant', description: 'Difficult to care for, but delicious', days_to_harvest: 120)
+plant3 = Plant.create!(name: 'Melon', description: 'Grows quickly, tastes good', days_to_harvest: 50)
+
+plot1.plants << plant1
+plot1.plants << plant2
+plot2.plants << plant3
+plot3.plants << plant1
+plot3.plants << plant3
