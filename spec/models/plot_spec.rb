7977418e22e7252703d1d07f4dfacafca3fa5b8f
Derek Chavez <frrenose17@yahoo.com>
2023-08-07 09:14:01 -0600

    feat: add relationship between plot and plotplants

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe Plot, type: :model do
   describe 'relationships' do
     it { should belong_to(:garden) }
+    it { should have_many(:plot_plants) }
   end
 end
