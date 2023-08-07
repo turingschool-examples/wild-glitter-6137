8a57e7313a914bd55fea2200dee44e6b3ace09a6
Weston Schutt <wtschutt@gmail.com>
2023-08-07 10:19:34 -0600

    Fix: Remove erroneous method and tests - no longer needed

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -2,8 +2,4 @@ class Plot < ApplicationRecord
    belongs_to :garden
    has_many :plot_plants
    has_many :plants, through: :plot_plants
-
-   def plant_names
-      plants.pluck(:name)
-   end
 end
