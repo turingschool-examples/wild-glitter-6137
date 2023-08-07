d4ba1b25c26ea62e58a406facfe18f37a563d6d7
Davis Weimer <davisweimer@gmail.com>
2023-08-07 11:56:25 -0600

    test: #plants_less_than_100

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Garden, type: :model do
+  before :each do
+    test_data
+  end
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should allow_value(%w(true false)).for(:organic) }
@@ -8,5 +11,13 @@ RSpec.describe Garden, type: :model do
 
   describe 'relationships' do
     it { should have_many(:plots) }
+  end
+
+  describe "instance methods" do
+    it "#plants_less_than_100" do
+      expect(@garden_1.plants_less_than_100).to match_array([@plant_1.name, @plant_2.name])
+      expect(@garden_2.plants_less_than_100).to match_array([@plant_1.name, @plant_3.name])
+      expect(@garden_3.plants_less_than_100).to match_array([@plant_1.name, @plant_2.name, @plant_3.name])
+    end
   end
 end
