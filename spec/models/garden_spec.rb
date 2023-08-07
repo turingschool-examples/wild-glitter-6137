8a90836bb5f5aeeedbdbcee5fac536819b90c3e5
Anna Wiley <awiley33@gmail.com>
2023-08-07 10:26:22 -0500

    chore: Set up db migrations and AR associations

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Garden, type: :model do
   describe 'relationships' do
     it { should have_many(:plots) }
+    it { should have_many(:plot_plants).through(:plots) }
+    it { should have_many(:plants).through(:plot_plants) }
   end
 end
---
+++ spec/models/plant_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Plant, type: :model do
+  describe 'relationships' do
+    it { should have_many(:plot_plants) }
+    it { should have_many(:plots).through(:plot_plants) }
+    it { should have_many(:gardens).through(:plots) }
+  end
+end
