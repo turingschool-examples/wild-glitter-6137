0a0e3742484a2aea404ba924f2b3d87d27b0199b
Dani Wilson <danirae.powell@gmail.com>
2023-08-07 09:32:10 -0600

    complete user story 1, add orderly gem

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -4,6 +4,18 @@ RSpec.describe "the plots index page" do
   before(:each) do
     test_data
   end
+  # user story 1
+  # As a visitor
+  # When I visit the plots index page ('/plots')
+  # I see a list of all plot numbers
+  # And under each plot number I see the names of all that plot's plants
+  it "displays a list of all plot numbers, with each plot's plants underneath" do
+    visit "/plots/index"
 
-  
+    expect(page).to have_content(30)
+    expect(page).to have_content(50)
+    expect(page).to have_content("Green Cabbage")
+    expect(page).to have_content("Cherry Tomato")
+    expect("Green Cabbage").to appear_before("Bell Pepper")
+  end
 end
