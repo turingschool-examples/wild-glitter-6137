8b45dd76c77a2ad2b241ab0ea82a48615c8e8ea3
Derek Chavez <frrenose17@yahoo.com>
2023-08-07 09:16:12 -0600

    test: add model test for plant relationships

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Plot, type: :model do
   describe 'relationships' do
     it { should belong_to(:garden) }
     it { should have_many(:plot_plants) }
+    it { should have_many(:plants).through(:plot_plants)}
   end
 end
