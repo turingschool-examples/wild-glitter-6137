704ef6a92c8528f9a9037cb7eb06813176652c40
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 12:07:02 -0400

    Test: adds test for user story 2

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -24,4 +24,45 @@ RSpec.describe 'Plots Index', type: :feature do
       end
     end
   end
+
+  # User Story 2, Remove a Plant from a Plot
+
+  # As a visitor
+  # When I visit the plots index page
+  # Next to each plant's name
+  # I see a link to remove that plant from that plot
+  # When I click on that link
+  # I'm returned to the plots index page
+  # And I no longer see that plant listed under that plot,
+  # And I still see that plant's name under other plots that is was associated with.
+
+  # NOTE: you do not need to test for any sad paths or implement any flash messages.
+
+  it 'has a link to remove a plant from a plot but still show that plant in another plot' do
+    visit plots_path
+
+    within("#plot-#{@plot1.id}") do
+      expect(page).to have_content(@plant1.name)
+    end
+
+    within("#plot-#{@plot2.id}") do
+      expect(page).to have_content(@plant1.name)
+    end
+
+    within("#plot-#{@plot1.id}") do
+      within("#plant_name_#{@plant1.id}") do
+        click_button 'Remove'
+      end
+    end
+
+    expect(current_path).to eq(plots_path)
+
+    within("#plot-#{@plot1.id}") do
+      expect(page).to_not have_content(@plant1.name)
+    end
+
+    within("#plot-#{@plot2.id}") do
+      expect(page).to have_content(@plant1.name)
+    end
+  end
 end
