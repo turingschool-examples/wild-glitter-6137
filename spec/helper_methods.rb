35b97ac2bb2aed0362b6a0cea1eaa250f8f83b24
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-07 12:39:16 -0400

    Test: updates helper_methods for better testing

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -1,6 +1,7 @@
 def load_test_data
   @garden1 = Garden.create!(name: 'Turing Community Garden', organic: true)
   @garden2 = Garden.create!(name: 'Main Street Garden', organic: false)
+  @garden3 = Garden.create!(name: 'Backyard Garden', organic: true)
 
   @plot1 = Plot.create!(number: 25, size: 'Large', direction: 'East', garden: @garden1)
   @plot2 = Plot.create!(number: 10, size: 'Small', direction: 'North', garden: @garden1)
@@ -9,9 +10,12 @@ def load_test_data
   @plant1 = Plant.create!(name: 'Tomato', description: 'Easy to care for, generates sauce', days_to_harvest: 90)
   @plant2 = Plant.create!(name: 'Eggplant', description: 'Difficult to care for, but delicious', days_to_harvest: 120)
   @plant3 = Plant.create!(name: 'Melon', description: 'Grows quickly, tastes good', days_to_harvest: 50)
+  @plant4 = Plant.create!(name: 'Corn', description: 'So many corns', days_to_harvest: 100)
 
   PlantPlot.create!(plant: @plant1, plot: @plot1)
   PlantPlot.create!(plant: @plant1, plot: @plot2)
   PlantPlot.create!(plant: @plant2, plot: @plot1)
   PlantPlot.create!(plant: @plant3, plot: @plot2)
+  PlantPlot.create!(plant: @plant4, plot: @plot1)
+  PlantPlot.create!(plant: @plant4, plot: @plot2)
 end
