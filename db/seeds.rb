7efb99ce69da08e764675729435535851e2935ea
Dani Wilson <danirae.powell@gmail.com>
2023-08-07 09:17:46 -0600

    create migrations and tables and associations and model tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  @garden_1 = Garden.create!(name: "Turing Community Garden", organic: true)
+  @garden_2 = Garden.create!(name: "Powell Family Garden", organic: true)
+
+  @plot_1 = @garden_1.plots.create!(number: 30, size: "Large", direction: "West")
+  @plot_2 = @garden_2.plots.create!(number: 50, size: "Extra Large", direction: "North")
+
+  @plant_1 = Plant.create!(name: "Bell Pepper", description: "This one's orange", days_to_harvest: 90)
+  @plant_2 = Plant.create!(name: "Radish", description: "It's a radish", days_to_harvest: 120)
+  @plant_3 = Plant.create!(name: "Cherry Tomato", description: "You'll have way too many of them to know what to do with", days_to_harvest: 65)
+  @plant_4 = Plant.create!(name: "Green Cabbage", description: "MY CABBAGES", days_to_harvest: 150)
+
+  PlantPlot.create!(plot_id: @plot_1.id, plant_id: @plant_4.id)
+  PlantPlot.create!(plot_id: @plot_1.id, plant_id: @plant_3.id)
+  PlantPlot.create!(plot_id: @plot_2.id, plant_id: @plant_2.id)
+  PlantPlot.create!(plot_id: @plot_2.id, plant_id: @plant_1.id)
---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "the plots index page" do
+  before(:each) do
+    test_data
+  end
+
+  
+end
