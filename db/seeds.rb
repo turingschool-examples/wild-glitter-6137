905453f6b79fa51c177aa563220de3ba64aa7f80
Connor Richmond <richmond.connor@gmail.com>
2023-08-07 13:45:58 -0400

    US3 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,20 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-@garden1 = Garden.create(name: 'Test Garden 1')
-@garden2 = Garden.create(name: 'Test Garden 2')
+@garden = Garden.create(name: 'Test Garden')
 
-
-@plot1 = @garden1.plots.create(number: 1)
-@plot2 = @garden1.plots.create(number: 2)
-@plot3 = @garden2.plots.create(number: 3)
+@plot1 = @garden.plots.create(number: 1)
+@plot2 = @garden.plots.create(number: 2)
+@plot3 = @garden.plots.create(number: 3)
 
 @plant1 = Plant.create(name: 'Plant 1', description: 'Description 1', days_to_harvest: 90)
 @plant2 = Plant.create(name: 'Plant 2', description: 'Description 2', days_to_harvest: 80)
+@plant3 = Plant.create(name: 'Plant 3', description: 'Description 3', days_to_harvest: 120)
 
 @plot1.plants << @plant1
 @plot2.plants << @plant1
 @plot2.plants << @plant2
+@plot3.plants << @plant1
+@plot3.plants << @plant3
 
 
--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -10,36 +10,31 @@ RSpec.describe 'Garden Show Page', type: :feature do
 
     @plant1 = Plant.create(name: 'Plant 1', description: 'Description 1', days_to_harvest: 90)
     @plant2 = Plant.create(name: 'Plant 2', description: 'Description 2', days_to_harvest: 80)
+    @plant3 = Plant.create(name: 'Plant 3', description: 'Description 3', days_to_harvest: 120)
 
     @plot1.plants << @plant1
     @plot2.plants << @plant1
     @plot2.plants << @plant2
+    @plot3.plants << @plant1
+    @plot3.plants << @plant3
 
     visit garden_path(@garden)
   end
 
-  # User Story 3, Garden's Plants
-
-  # As a visitor
-  # When I visit a garden's show page ('/gardens/:id')
-  # Then I see a list of plants that are included in that garden's plots
-  # And I see that this list is unique (no duplicate plants)
-  # And I see that this list only includes plants that take less than 100 days to harvest
-
   it 'lists plants included in the garden plots with less than 100 days to harvest' do
     expect(page).to have_content(@garden.name)
 
-    within('#plants-list') do
-      expect(page).to have_content(@plant1.name)
-      expect(page).to have_content(@plant2.name)
-      expect(page).not_to have_content(@plant3.name)
-    end
+    expect(page).to have_content(@plant1.name)
+    expect(page).to have_content(@plant2.name)
+    expect(page).not_to have_content(@plant3.name)
   end
 
   it 'lists only unique plants' do
     # Assuming the same plant is added to different plots
-    expect(page).to have_selector('#plants-list li', count: 2)
+    plants = [@plant1.name, @plant2.name]
+
+    plants.each do |plant_name|
+      expect(page).to have_text(plant_name, count: 1)
+    end
   end
-
-
 end
